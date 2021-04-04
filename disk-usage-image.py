62fd34ce0d6ae90017d98e11e2c6ba9e407839cb
hy-son <leopold.le_roux@protonmail.com>
2021-04-04 23:18:32 +0200

    Correct display bug. The image was hard to read, now texts are bigger.

--- disk-usage-image.py
+++ disk-usage-image.py
@@ -23,13 +23,13 @@ sizes = [disk_free_percent, disk_used_percent]
 explode = (0, 0.1)  # only "explode" the 2nd slice (i.e. 'Hogs')
 colors = ["#99ff99","#ff9999"]
 
-fig, ax = plt.subplots(2,1,figsize=(16,9), gridspec_kw={'height_ratios': [5, 1]})
-ax[0].set_title("Disk usage", family='Arial', fontsize=20, fontweight='bold')
-ax[0].pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%', shadow=False, startangle=90, colors= colors, labeldistance=0.1, textprops={'fontsize': 20} ) 
+fig, ax = plt.subplots(2,1,figsize=(16,16), gridspec_kw={'height_ratios': [5, 1]})
+ax[0].set_title("Disk usage", family='Arial', fontsize=50, fontweight='bold')
+ax[0].pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%', shadow=False, startangle=90, colors= colors, labeldistance=0.1, textprops={'fontsize': 40} ) 
 ax[0].axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
 
 
-ax[1].set_title("/boot usage", family='Arial', fontsize=20, fontweight='bold')
+ax[1].set_title("/boot usage", family='Arial', fontsize=50, fontweight='bold')
 ax[1].barh(0.2,boot_used_percent, align='center', color= colors[1], label="Used" )
 ax[1].barh(0.2,boot_free_percent, left= boot_used_percent+1, align='center',color= colors[0], label="Free" )
 ax[1].axis("off")
