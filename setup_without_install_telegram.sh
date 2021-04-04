127078b09774fe9f76e5f89d573e0a8d1b94c3d6
hy-son <leopold.le_roux@protonmail.com>
2021-04-04 19:53:35 +0200

    Add install without clearning telegram configuration

--- setup_without_install_telegram.sh
+++ setup_without_install_telegram.sh
@@ -9,10 +9,6 @@ cp * /opt/TardisLink/
 
 cd /opt/TardisLink
 
-# Install telegram script from Nicolas Bernaerts
-wget https://raw.githubusercontent.com/NicolasBernaerts/debian-scripts/master/telegram/telegram-notify-install.sh
-./telegram-notify-install.sh
-
 # Loging alert
 chmod +x telegram-login-msg.sh
 cp telegram-login-msg.sh /etc/profile.d/telegram-login-msg.sh
